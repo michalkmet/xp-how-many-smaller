3165ba864a6faf4a4270a70f3b5321e3d99f6a73
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-13 10:12:37 +0200

    10th test pass

--- src/how-many-smaller.test.js
+++ src/how-many-smaller.test.js
@@ -35,3 +35,9 @@ describe('Story 3 user test cases', () => {
     expect(smaller([2,2])).toEqual([0,0]);
   });
 });
+
+describe('Story 4 user test cases', () => {
+  it('when given [0,1], it should return [0,0]', () => {
+    expect(smaller([0,1])).toEqual([0,0]);
+  });
+});
